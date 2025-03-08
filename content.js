45efc8da39c5e5bea29ec178a8aca40e554bfecc
mdave88 <david.magyari.88@gmail.com>
2025-03-08 22:37:03 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,3 @@
-// Function to extract JSON-LD data from the page
 function getJSONLDData() {
     const scripts = document.querySelectorAll('script[type="application/ld+json"]');
     for (let script of scripts) {
@@ -14,23 +13,18 @@ function getJSONLDData() {
     return null;
 }
 
-// Get the book data from JSON-LD
 const bookData = getJSONLDData();
 if (bookData) {
     const title = bookData.name;
     const author = bookData.author && bookData.author[0] ? bookData.author[0].name : null;
 
     if (title && author) {
-        console.log('Book Title:', title);
-        console.log('Book Author:', author);
-
-        // Create the search button
         const button = document.createElement('button');
         button.textContent = 'Download';
         button.style.position = 'fixed';
-        button.style.top = '10px';
-        button.style.right = '10px';
-        button.style.padding = '10px';
+        button.style.top = '5px';
+        button.style.right = '5px';
+        button.style.padding = '5px';
         button.style.backgroundColor = '#0a74da';
         button.style.color = '#fff';
         button.style.border = 'none';
@@ -38,15 +32,13 @@ if (bookData) {
         button.style.cursor = 'pointer';
         button.style.zIndex = 10000;
 
-        // Append the button to the body
         document.body.appendChild(button);
 
-        // Add click event to the button to open Anna's Archive search
         button.addEventListener('click', () => {
 			window.open(`https://annas-archive.org/search?index=&page=1&q=${encodeURIComponent(`${title} ${author}`)}&display=&sort=`, '_blank');
 			window.open(`https://z-lib.id/s?q=${encodeURIComponent(`${title}`)}`, '_blank');
 			window.open(`https://z-lib.io/s/${encodeURIComponent(`${title}`)}`, '_blank');
-	});
+		});
     } else {
         console.log('Could not find title or author in the JSON-LD.');
     }
